f12effccaa6be9939dcf13ddf04d1a41691230c4
Muhamad Alzika <zikafakk@gmail.com>
2024-10-10 19:21:24 +0700

    firstly commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Random dice = new Random();
+
+int daysUntilExiration = 0;
+int discountPercentage = 0;
+
+if (daysUntilExiration == 0) {
+    Console.WriteLine("Your subscription has expired.");
+} else if ( daysUntilExiration == 1) {
+    Console.WriteLine("Your subscription expires within a day!");
+    discountPercentage = 20;
+    Console.WriteLine("Renew now and save "+discountPercentage+ "%!");
+} else if (daysUntilExiration >= 0 && daysUntilExiration <=5) {
+    Console.WriteLine("Your subscription expires in " +daysUntilExiration+ "days.");
+    discountPercentage = 10;
+    Console.WriteLine("Renew now and save "+discountPercentage+"%!");
+} else if (daysUntilExiration >= 5 && daysUntilExiration <= 10) {
+    Console.WriteLine("Your subscription expiresn soon. Renew now!");
+}
